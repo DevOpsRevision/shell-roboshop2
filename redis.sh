4b04338b9affdce8a6d244e7d1d3c515b482795c
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-03-06 18:09:34 -0500

    cart user redis setup update

--- redis.sh
+++ redis.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
+APP_NAME="redis"
 
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(basename "$0" | cut -d "." -f 1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-
-mkdir -p "$LOGS_FOLDER"
-
-if [ $USERID -ne 0 ]; then
-  echo -e "$R ERROR :: You are NOT root user. Please run this script with root priviliges. $N" | tee -a $LOG_FILE
-  exit 1 # Exit with a non-zero status to indicate an error
-else
-  echo -e "$G INFO :: You are root user. Proceeding with the script execution. $N" | tee -a $LOG_FILE
-fi
-
-VALIDATE(){
-  if [ $1 -ne 0 ]; then
-    echo -e "$R ERROR :: $2 installation .... $R FAILURE $N" | tee -a $LOG_FILE
-    exit 1 # Exit with a non-zero status to indicate an error
-  else
-    echo -e "$G INFO :: $2 installation .... $G SUCCESSFUL. $N" | tee -a $LOG_FILE
-  fi
-}
+CHECK_ROOT
 
 dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling Redis Module"
