aeea26e55e1de835598f8f8d9154a8569e0406de
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-03-06 18:34:49 -0500

    Rabbitmq Payment setup update

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -17,8 +17,13 @@ VALIDATE $? "Enabling RabbitMQ Service"
 systemctl start rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Starting RabbitMQ Service"
 
-rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
-VALIDATE $? "Adding RabbitMQ User"
+if ! rabbitmqctl list_users | grep -q "^roboshop"
+then
+    rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+    VALIDATE $? "Adding RabbitMQ User"
+else
+    echo -e "$G INFO :: RabbitMQ user 'roboshop' already exists. Skipping user creation. $N" | tee -a $LOG_FILE
+fi
 
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 VALIDATE $? "Setting RabbitMQ User Permissions"
